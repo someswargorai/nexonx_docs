7c47a0c836d1beb18f1b9ec5c3e6c4d0977a2348
someswargorai <goraisomeswar@gmail.com>
2026-06-21 01:52:12 +0530

    feat:enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -82,6 +83,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
       suppressHydrationWarning
     >
+      <Analytics/>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
